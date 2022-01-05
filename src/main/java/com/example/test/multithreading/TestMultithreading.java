185c59826d97e7be89349a75f6e57ced4b4a5620
Shalabh Chaturvedi <shalabhchaturvedi@192.168.1.36>
2022-01-05 22:06:10 +0530

    comment join() method

--- src/main/java/com/example/test/multithreading/TestMultithreading.java
+++ src/main/java/com/example/test/multithreading/TestMultithreading.java
@@ -8,14 +8,14 @@ public class TestMultithreading {
             MultithreadingClass multithreadingClass = new MultithreadingClass(i);
             Thread myThread = new Thread(multithreadingClass);
             myThread.start();
-            try {
-                /*join method forces the jvm to hold other threads execution
+            /*try {
+                *//*join method forces the jvm to hold other threads execution
                     until current thread completes it's execution.
-                 */
+                 *//*
                 myThread.join();
             } catch (InterruptedException e) {
                 e.printStackTrace();
-            }
+            }*/
         }
     }
 }
